fb5fa039627c618f783174ec248ad3e913571bd3
filwab <3350354841@qq.com>
2023-11-15 03:30:53 +0000

    update ignore and add bear in run_trace.sh

--- code/run_tracest.sh
+++ code/run_tracest.sh
@@ -5,7 +5,7 @@ chunksize=64 # set the chunk size of straid to (chunksize * KB)
 
 sed -in-place -e '/^#define NUM_THREADS/ c \#define NUM_THREADS ('${thread}")" ./include/define.h
 sed -in-place -e '/^#define SCHUNK_SIZE/ c \#define SCHUNK_SIZE ('${chunksize}" * KB)" ./include/define.h
-make -j8
+bear make -j8
 
 echo "" > ./results/ST_trace_results.txt
 
